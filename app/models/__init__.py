4caabc32bde66abe7aa11d76d2012c3363ef06ca
Brian P <palmucci.city.council@gmail.com>
2025-10-25 22:27:58 -0400

    Add missing model imports (PlayerContract, TradeProposal, etc.)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,10 @@ from .player import Player
 from .depth_chart import DepthChart
 from .game_result import GameResult
 from .draft import DraftPickInventory  # noqa
+from .contract_models import PlayerContract, CapSummary  # noqa
+from .trade import TradeProposal  # noqa
+from .trade_block import TeamTradeBlock  # noqa
+from .event_log import EventLog  # noqa
 
 __all__ = ["Team", "Conference", "Division", "Player", "DepthChart", "GameResult"]
 
